34f50b5b7a6cc4353718da1711182a39a36db3c4
kometbomb@gmail.com <kometbomb@gmail.com@61cf05b0-a575-11de-b950-3d5b5f4ea0aa>
2011-11-25 13:55:11 +0000

    DLL library wrapper code

---
+++ src/lib/ksnd.h
@@ -0,0 +1,28 @@
+#ifndef KSND_DLL_H
+
+/*
+
+Wrapper library for easy klystron song loading and playing.
+
+*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct KSong_t KSong;
+typedef struct KPlayer_t KPlayer;
+
+KSong* KSND_LoadSong(KPlayer* player, const char *path);
+KSong* KSND_LoadSongFromMemory(KPlayer* player, void *data, int data_size);
+void KSND_FreeSong(KSong *song);
+KPlayer* KSND_CreatePlayer(int sample_rate);
+void KSND_FreePlayer(KPlayer *player);
+void KSND_PlaySong(KPlayer *player, KSong *song);
+void KSND_Stop(KPlayer* player);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
