e04a30a6756e39a32f1c6c00c2a09aff51072e9b
Tero Lindeman <kometbomb@kometbomb.net>
2019-04-20 16:22:20 +0300

    libksnd fill buffer/native playback did not zero buffer

--- src/lib/ksnd.c
+++ src/lib/ksnd.c
@@ -7,14 +7,14 @@
 #include <string.h>
 
 
-struct KSong_t 
+struct KSong_t
 {
 	MusSong song;
 	CydWavetableEntry wavetable_entries[CYD_WAVE_MAX_ENTRIES];
 };
 
 
-struct KPlayer_t 
+struct KPlayer_t
 {
 	CydEngine cyd;
 	MusEngine mus;
@@ -25,13 +25,13 @@ struct KPlayer_t
 KLYSAPI KSong* KSND_LoadSong(KPlayer* player, const char *path)
 {
 	KSong *song = calloc(sizeof(*song), 1);
-	
+
 	int i = 0;
 	for (i = 0 ; i < CYD_WAVE_MAX_ENTRIES ; ++i)
 	{
 		cyd_wave_entry_init(&song->wavetable_entries[i], NULL, 0, 0, 0, 0, 0);
 	}
-	
+
 	if (mus_load_song(path, &song->song, song->wavetable_entries))
 	{
 		return song;
@@ -48,9 +48,9 @@ static int RWread(struct RWops *context, void *ptr, int size, int maxnum)
 {
 	const int len = my_min(size * maxnum, context->mem.length - context->mem.ptr);
 	memcpy(ptr, context->mem.base + context->mem.ptr, len);
-	
+
 	context->mem.ptr += len;
-	
+
 	return len;
 }
 
@@ -74,15 +74,15 @@ KLYSAPI KSong* KSND_LoadSongFromMemory(KPlayer* player, void *data, int data_siz
 #else
 	RWops *ops = SDL_RWFromMem(data, data_size);
 #endif
-	
+
 	KSong *song = calloc(sizeof(*song), 1);
-	
+
 	int i = 0;
 	for (i = 0 ; i < CYD_WAVE_MAX_ENTRIES ; ++i)
 	{
 		cyd_wave_entry_init(&song->wavetable_entries[i], NULL, 0, 0, 0, 0, 0);
 	}
-	
+
 	if (mus_load_song_RW(ops, &song->song, song->wavetable_entries))
 	{
 		return song;
@@ -122,7 +122,7 @@ KLYSAPI int KSND_GetSongLength(const KSong *song)
 KLYSAPI KPlayer* KSND_CreatePlayer(int sample_rate)
 {
 	KPlayer *player = KSND_CreatePlayerUnregistered(sample_rate);
-	
+
 	player->cyd_registered = true;
 
 #ifdef NOSDL_MIXER
@@ -130,23 +130,23 @@ KLYSAPI KPlayer* KSND_CreatePlayer(int sample_rate)
 #else
 	cyd_register(&player->cyd);
 #endif
-	
+
 	return player;
 }
 
 KLYSAPI KPlayer* KSND_CreatePlayerUnregistered(int sample_rate)
 {
 	KPlayer *player = malloc(sizeof(*player));
-	
+
 	player->cyd_registered = false;
-	
+
 	cyd_init(&player->cyd, sample_rate, 1);
 	mus_init_engine(&player->mus, &player->cyd);
-	
+
 	// Each song has its own wavetable array so let's free this
-	free(player->cyd.wavetable_entries); 
+	free(player->cyd.wavetable_entries);
 	player->cyd.wavetable_entries = NULL;
-	
+
 	return player;
 }
 
@@ -160,10 +160,10 @@ KLYSAPI void KSND_SetPlayerQuality(KPlayer *player, int oversample)
 KLYSAPI void KSND_FreePlayer(KPlayer *player)
 {
 	KSND_Stop(player);
-	
-	if (player->cyd_registered) 
+
+	if (player->cyd_registered)
 		cyd_unregister(&player->cyd);
-		
+
 	cyd_deinit(&player->cyd);
 	free(player);
 }
@@ -174,16 +174,19 @@ KLYSAPI void KSND_PlaySong(KPlayer *player, KSong *song, int start_position)
 	player->cyd.wavetable_entries = song->wavetable_entries;
 	cyd_set_callback(&player->cyd, mus_advance_tick, &player->mus, song->song.song_rate);
 	mus_set_fx(&player->mus, &song->song);
-	
+
 	if (song->song.num_channels > player->cyd.n_channels)
 		cyd_reserve_channels(&player->cyd, song->song.num_channels);
-	
+
 	mus_set_song(&player->mus, &song->song, start_position);
 }
 
 
 KLYSAPI int KSND_FillBuffer(KPlayer *player, short int *buffer, int buffer_length)
 {
+  // Zero buffer as klystron will mix the new data with existing buffer contents
+  memset(buffer, 0, buffer_length);
+
 #ifdef NOSDL_MIXER
 	cyd_output_buffer_stereo(&player->cyd, (void*)buffer, buffer_length);
 #else
@@ -211,9 +214,9 @@ KLYSAPI void KSND_Pause(KPlayer *player, int state)
 KLYSAPI int KSND_GetPlayPosition(KPlayer *player)
 {
 	int song_position = 0;
-	
+
 	mus_poll_status(&player->mus, &song_position, NULL, NULL, NULL, NULL, NULL, NULL);
-	
+
 	return song_position;
 }
 
@@ -244,10 +247,10 @@ KLYSAPI const KSongInfo * KSND_GetSongInfo(KSong *song, KSongInfo *data)
 	data->song_title = song->song.title;
 	data->n_instruments = song->song.num_instruments;
 	data->n_channels = song->song.num_channels;
-	
+
 	for (int i = 0 ; i < data->n_instruments ; ++i)
 		data->instrument_name[i] = song->song.instrument[i].name;
-		
+
 	return data;
 }
 
