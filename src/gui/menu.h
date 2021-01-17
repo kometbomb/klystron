c47ac62878d7ecf16adbfe2b8420798b094a9b9b
Emanuel Haupt <ehaupt@critical.ch>
2021-01-17 20:25:08 +0100

    Fix build with clang11
    Fix build with clang11 otherwise you get:
    
    ```
    ld: error: duplicate symbol: menu_
    ```

--- src/gui/menu.h
+++ src/gui/menu.h
@@ -25,7 +25,7 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 */
 
-struct menu_t
+extern struct menu_t
 {
 	int flags;
 	const struct menu_t *parent;
